228e1e448cf57fd3c80b91e4693670cdef1b1e79
Stéphane Leite <sleite1910@gmail>
2026-06-09 16:22:54 +0200

    exam2

--- script/exo_3.js
+++ script/exo_3.js
@@ -1,14 +1,29 @@
 
+function createElementByTagName(tagName, ...classList) {
+    const el = document.createElement(tagName);
+    for (const classItem of classList) {
+        el.classList.add(classItem);
+    }
+    return el;
 
+}
 
 window.addEventListener('load', () => {
 
-    const newTask = document.querySelector('section>div');
+    const newTask = document.querySelector('button');
+    const input = document.querySelector('input')
     const section = document.querySelector('section>ul')
 
-    newTask.addEventListener('submit', (event) => {
-        const value = newTask.value
-        console.log(value)
+    newTask.addEventListener('click', (event) => {
+        event.preventDefault();
+        const value = input.value
+        const toDo = createElementByTagName('li', 'text-to-do');
+        section.appendChild(toDo)
+        toDo.innerHTML = value
+
+        if (value === '') {
+            alert("Remplir l'input pour ajouter")
+        }
 
     })
 
